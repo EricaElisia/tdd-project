eff47c7c21642d7721ef8d626e8a2fb12b5d1540
Érica Elísia Bueno Lemos <Erica55362396@edu.df.senac.br>
2026-04-08 21:20:42 -0300

    Utilizando o módulo unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,21 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'To-Do' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        
+        # Maria entra no site
+        self.browser.get('http://localhost:8000')
+
+        # Ela nota o título
+        self.assertIn('To-Do', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main()
